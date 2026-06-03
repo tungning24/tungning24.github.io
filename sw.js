c84f1c8e8186ef0b192fc5eb5abbe83987898c73
tungning24 <tungning.p@gmail.com>
2026-06-03 20:31:52 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "myapps-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./projects.json",
+  "./manifest.json",
+  "./icon-192x192.png",
+  "./icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
